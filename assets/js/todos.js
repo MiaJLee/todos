94047f370e4f850665bea06ea862e4df20a93e7b
MiaJLee <48678660+MiaJLee@users.noreply.github.com>
2020-07-25 01:09:05 +0900

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,44 @@
+// 클릭해서 TODO 가로선 그리기
+
+var list = document.querySelector("ul");
+
+list.addEventListener("click", function(event){
+	if (event.target.tagName === "LI") {
+		event.target.classList.toggle("completed");
+	}
+}, false);
+
+// span 클릭시 TODO 삭제하기
+list.addEventListener("click", function(event){
+	if (event.target.tagName === "SPAN") {
+		var parent = event.target.parentNode;
+		parent.remove();
+	}
+})
+
+// input 설정
+var input = document.querySelector("input[type='text']")
+
+input.addEventListener("keypress", function(event){
+	if (event.which === 13){
+		// input 에서 텍스트 가져오기
+		var todoText = this.value;
+		// 새로운 li 추가하기
+		var newLi = "<li><span><i class=\"fa fa-trash-alt\"></i></span> " + todoText +"</li>";
+		list.innerHTML += newLi;
+		input.value = "";
+	}
+})
+
+// + 아이콘 토글
+var plus = document.querySelector(".fa-plus")
+
+plus.addEventListener("click", function(event){
+	if (input.style.display === "none") {
+		input.style.display = "block";
+		event.target.classList.toggle("rotated")
+	} else {
+		input.style.display = "none";
+		event.target.classList.toggle("rotated")
+	}
+})
