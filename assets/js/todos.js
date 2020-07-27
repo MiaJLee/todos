09686c4ebff11c0c9861c323bdbf4682192e9537
MiaJLee <48678660+MiaJLee@users.noreply.github.com>
2020-07-27 19:40:48 +0900

    Update todos.js
    delete all todos 버튼 추가,
    localStorage 추가

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,10 @@
 // 클릭해서 TODO 가로선 그리기
-
 var list = document.querySelector("ul");
 
 list.addEventListener("click", function(event){
 	if (event.target.tagName === "LI") {
 		event.target.classList.toggle("completed");
+		store();
 	}
 }, false);
 
@@ -13,6 +13,7 @@ list.addEventListener("click", function(event){
 	if (event.target.tagName === "SPAN") {
 		var parent = event.target.parentNode;
 		parent.remove();
+		store();
 	}
 })
 
@@ -21,11 +22,13 @@ var input = document.querySelector("input[type='text']")
 
 input.addEventListener("keypress", function(event){
 	if (event.which === 13){
+		console.log("new list")
 		// input 에서 텍스트 가져오기
 		var todoText = this.value;
 		// 새로운 li 추가하기
 		var newLi = "<li><span><i class=\"fa fa-trash-alt\"></i></span> " + todoText +"</li>";
 		list.innerHTML += newLi;
+		store();
 		input.value = "";
 	}
 })
@@ -42,3 +45,27 @@ plus.addEventListener("click", function(event){
 		event.target.classList.toggle("rotated")
 	}
 })
+
+// delete all Todos
+var clear = document.querySelector(".fa-trash")
+
+clear.addEventListener("click", function(event){
+	console.log("clear the list")
+	while (list.hasChildNodes()) {
+		list.removeChild(list.firstChild);
+	}
+	store();
+}, false);
+
+// localStorage 활용하기
+function store() {
+	window.localStorage.myitems = list.innerHTML;
+}
+
+function getValues() {
+	var storedValues = window.localStorage.myitems;
+	if(storedValues) {
+		list.innerHTML = storedValues;
+	}
+}
+getValues();
